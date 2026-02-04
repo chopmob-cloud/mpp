9c12aca991efe7f52554f63242e242f8f5589c8d
Brendan Ryan <brendan@bridge.xyz>
2026-02-04 07:29:57 -0800

    fix: use testnet option instead of explicit chainId/rpcUrl

--- src/mpay.server.ts
+++ src/mpay.server.ts
@@ -1,12 +1,8 @@
 import { env } from "cloudflare:workers";
 import { Mpay, tempo } from "mpay/server";
-import { tempoModerato } from "viem/chains";
 
 export const mpay = Mpay.create({
-	method: tempo({
-		chainId: tempoModerato.id,
-		rpcUrl: tempoModerato.rpcUrls.default.http[0],
-	}),
+	method: tempo({ testnet: true }),
 	realm: "mpp.dev",
 	secretKey: env.SECRET_KEY! ?? "top-secret",
 });
